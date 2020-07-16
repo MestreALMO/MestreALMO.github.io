b8f814baf32371466b89fe1c9bbaa34092359bec
André Lusegardis <lusegardis@gmail.com>
2020-07-16 00:53:36 -0300

    Experience translation update

--- assets/js/navbar_translation.js
+++ assets/js/navbar_translation.js
@@ -16,6 +16,40 @@ var arrLang = {
         "skills.title": "Skills",
         "skills.programming": "Programming",
         "skills.others": "Others",
+        "skills.english": "English",
+        "skills.portuguese": "Portuguese",
+
+        "experience.title": "Experience",
+        "experience.DeSoftDate": "04/2020 - Currently",
+        "experience.DeSoftLocation": "Natal, RN. Brazil.",
+        "experience.DeSoftPosition": "Programmer, Fullstacker Focused.",
+        "experience.DeSoftText": "I am doing lots here in their system, some of the activities are: Fixing bugs, integrating the Uber Eats API, adjusting the intire system to becoming responsive, making from scratch a new layout for the kitchen, etc. The work tools were: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. I am working with: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
+        /** */
+        "experience.ARConsultoriaLocation": "Natal, RN. Brazil.",
+        "experience.ARConsultoriaPosition": "System Analyst, Front-ending Focused.",
+        "experience.ARConsultoriaText": "Some of the things i worked on: constructing the page for the principal product of the company, the AR PLUS; I also extracted many informations from the data base to use on new graphics, I fixed lot's of layouts, corrected some bugs.The used work tools were: PHP, HTML, MySQL, CSS, Bootstrap, JavaScript, JQuery, Adianti, Highcharts, TortoiseSVN.",
+        /** */
+        "experience.SMSLocation": "Natal, RN. Brazil.",
+        "experience.SMSPosition": "System Analyst, Front-ending Focused. Paid internship.",
+        "experience.SMSText": "Internship in Secretaria Municipal de Saúde (Municipal Health Secretariat), the work got my knowledge stronger in: PHP, HTML, JavaScript, CSS, JQuerry, SQL. I worked fixing the existent systems and the help i had was by messages, since the person that could assist me was work from other places.",
+        /** */
+        "experience.SEPLANLocation": "Centro Administrativo de Natal, RN. Brazil.",
+        "experience.SEPLANPosition": "Data Analyst. Paid internship.",
+        "experience.SEPLANText": "Experience with Power BI and extraction of data from the data base of SEPLAN, Secretaria de Estado de Planejamento e Finanças (State Secretariat for Planning and Finance), of centro Administrativo de Natal, RN (Administrative center of Natal, RN). The search in the data base was always done in MySQL.",
+        /** */
+        "experience.UNP1Location": "Natal, RN. Brazil.",
+        "experience.UNP1Position": "System Analyst, Front-ending Focused. Unpaid internship & Paid internship.",
+        "experience.UNP1Text": "This is the Univerdidade Potiguar, or Potiguar University in English. I participated in E-code projects, there were projects related to hospitals, to security and other minors projects, all of them were really put in use, and the financial return came back proportion a better pay to internships. The work tool were: PHP, Adianti Framework, MySQL.",
+        /** */
+        "experience.UNP2Location": "Natal, RN. Brazil.",
+        "experience.UNP2Position": "Teacher and monitor. Voluntier.",
+        "experience.UNP2Text": "I patcipated of the interity of the project Informática Cidadã (citizen computing), where I teached basic computing, Windows 10 and Microsoft Office to mostly disabled people, some were hearing impaired, some visual deficiency, among some others.",
+        /** */
+        "experience.GameVicioDate": "Around 2010",
+        "experience.GameVicioLocation": "At the time the site link was: <br>www.gamevicio.com.br",
+        "experience.GameVicioPosition": "Translator. Voluntier.",
+        "experience.GameVicioText": "Voluteer work as a translator of games from english to portuguese of site gamevicio.com (between the games I helped traslate there was: Deux Ex and Fable 3). I participated in projects for a period around 2 years.",
+
     },
     "portuguese": {
         "navbar.home": "Home",
@@ -34,6 +68,39 @@ var arrLang = {
         "skills.title": "Habilidades",
         "skills.programming": "Programação",
         "skills.others": "Outras",
+        "skills.english": "Inglês",
+        "skills.portuguese": "Português",
+
+        "experience.title": "Experiência",
+        "experience.DeSoftDate": "04/2020 - Atualmente",
+        "experience.DeSoftLocation": "Natal, RN. Brasil.",
+        "experience.DeSoftPosition": "Programador, focado em Fullstack.",
+        "experience.DeSoftText": "I am doing lots of works here in their system, some of the activities are: Fixing bugs, integrating the Uber Eats API, adjusting the intire system to becoming responsive, making from scratch a new layout for the kitchen, etc. The work tools were: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. I am working with: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
+        /** */
+        "experience.ARConsultoriaLocation": "Natal, RN. Brasil.",
+        "experience.ARConsultoriaPosition": "Analista de Sistemas, Foco em Front-ending.",
+        "experience.ARConsultoriaText": "Some of the things i worked on: constructing the page for the principal product of the company, the AR PLUS; I also extracted many informations from the data base to use on new graphics, I fixed lot's of layouts, corrected some bugs.The used work tools were: PHP, HTML, MySQL, CSS, Bootstrap, JavaScript, JQuery, Adianti, Highcharts, TortoiseSVN.",
+        /** */
+        "experience.SMSLocation": "Natal, RN. Brasil.",
+        "experience.SMSPosition": "Analista de Sistemas, Foco em Front-ending. Estágio remunerado.",
+        "experience.SMSText": "Internship in Secretaria Municipal de Saúde (Municipal Health Secretariat), the work got my knowledge stronger in: PHP, HTML, JavaScript, CSS, JQuerry, SQL. I worked fixing the existent systems and the help i had was by messages, since the person that could assist me was work from other places.",
+        /** */
+        "experience.SEPLANLocation": "Centro Administrativo de Natal, RN. Brasil.",
+        "experience.SEPLANPosition": "Analista de dados. Estágio remunerado.",
+        "experience.SEPLANText": "Experience with Power BI and extraction of data from the data base of SEPLAN, Secretaria de Estado de Planejamento e Finanças (State Secretariat for Planning and Finance), of centro Administrativo de Natal, RN (Administrative center of Natal, RN). The search in the data base was always done in MySQL.",
+        /** */
+        "experience.UNP1Location": "Natal, RN. Brasil.",
+        "experience.UNP1Position": "Analista de Sistemas, Foco em Front-ending. Estágio não remunerado & estágio remunerado.",
+        "experience.UNP1Text": "This is the Univerdidade Potiguar, or Potiguar University in English. I participated in E-code projects, there were projects related to hospitals, to security and other minors projects, all of them were really put in use, and the financial return came back proportion a better pay to internships. The work tool were: PHP, Adianti Framework, MySQL.",
+        /** */
+        "experience.UNP2Location": "Natal, RN. Brasil.",
+        "experience.UNP2Position": "Teacher and monitor. Voluntier.",
+        "experience.UNP2Text": "I patcipated of the interity of the project Informática Cidadã (citizen computing), where I teached basic computing, Windows 10 and Microsoft Office to mostly disabled people, some were hearing impaired, some visual deficiency, among some others.",
+        /** */
+        "experience.GameVicioDate": "Around 2010",
+        "experience.GameVicioLocation": "At the time the site link was: <br>www.gamevicio.com.br",
+        "experience.GameVicioPosition": "Translator. Voluntier.",
+        "experience.GameVicioText": "Voluteer work as a translator of games from english to portuguese of site gamevicio.com (between the games I helped traslate there was: Deux Ex and Fable 3). I participated in projects for a period around 2 years.",
     }
 };
 
