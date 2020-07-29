7fe16eeb90b4dd5a05dd757879c121683c4932ec
André Lusegardis <lusegardis@gmail.com>
2020-07-29 10:02:00 -0300

    translation of experience portuguese completed

--- assets/js/navbar_translation.js
+++ assets/js/navbar_translation.js
@@ -23,7 +23,7 @@ var arrLang = {
         "experience.DeSoftDate": "04/2020 - Currently",
         "experience.DeSoftLocation": "Natal, RN. Brazil.",
         "experience.DeSoftPosition": "Programmer, Fullstacker Focused.",
-        "experience.DeSoftText": "I am doing lots here in their system, some of the activities are: Fixing bugs, integrating the Uber Eats API, adjusting the intire system to becoming responsive, making from scratch a new layout for the kitchen, etc. The work tools were: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. I am working with: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
+        "experience.DeSoftText": "I am doing lots here in their system, some of the activities are: Fixing bugs, integrating the Uber Eats API, adjusting the a lot of part of the system to becoming responsive, making from scratch a new layout for the kitchen, etc. The work tools are: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. I am working with: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
         /** */
         "experience.ARConsultoriaLocation": "Natal, RN. Brazil.",
         "experience.ARConsultoriaPosition": "System Analyst, Front-ending Focused.",
@@ -31,7 +31,7 @@ var arrLang = {
         /** */
         "experience.SMSLocation": "Natal, RN. Brazil.",
         "experience.SMSPosition": "System Analyst, Front-ending Focused. Paid internship.",
-        "experience.SMSText": "Internship in Secretaria Municipal de Saúde (Municipal Health Secretariat), the work got my knowledge stronger in: PHP, HTML, JavaScript, CSS, JQuerry, SQL. I worked fixing the existent systems and the help i had was by messages, since the person that could assist me was work from other places.",
+        "experience.SMSText": "Internship in Secretaria Municipal de Saúde (Municipal Health Secretariat), the work got my knowledge stronger in: PHP, HTML, JavaScript, CSS, JQuerry, SQL. I worked fixing and upgrading the existent systems, a important thing here is that the help i had was by messages, since the person that could assist me was working from other places.",
         /** */
         "experience.SEPLANLocation": "Centro Administrativo de Natal, RN. Brazil.",
         "experience.SEPLANPosition": "Data Analyst. Paid internship.",
@@ -39,7 +39,7 @@ var arrLang = {
         /** */
         "experience.UNP1Location": "Natal, RN. Brazil.",
         "experience.UNP1Position": "System Analyst, Front-ending Focused. Unpaid internship & Paid internship.",
-        "experience.UNP1Text": "This is the Univerdidade Potiguar, or Potiguar University in English. I participated in E-code projects, there were projects related to hospitals, to security and other minors projects, all of them were really put in use, and the financial return came back proportion a better pay to internships. The work tool were: PHP, Adianti Framework, MySQL.",
+        "experience.UNP1Text": "This is the Universidade Potiguar, or Potiguar University in English. I participated in E-code projects, there were lot's of projects, there was one related to hospitals, other to security and other projects, all of them were really put in use, and the financial return came back proportion a better pay to internships. The work tool were: PHP, Adianti Framework, MySQL.",
         /** */
         "experience.UNP2Location": "Natal, RN. Brazil.",
         "experience.UNP2Position": "Teacher and monitor. Voluntier.",
@@ -75,32 +75,32 @@ var arrLang = {
         "experience.DeSoftDate": "04/2020 - Atualmente",
         "experience.DeSoftLocation": "Natal, RN. Brasil.",
         "experience.DeSoftPosition": "Programador, focado em Fullstack.",
-        "experience.DeSoftText": "I am doing lots of works here in their system, some of the activities are: Fixing bugs, integrating the Uber Eats API, adjusting the intire system to becoming responsive, making from scratch a new layout for the kitchen, etc. The work tools were: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. I am working with: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
+        "experience.DeSoftText": "Faço diversos trabalhos aqui no sistema, algumas das atividades são: ajeitando bugs, integrando a Uber Eats API, ajustando diversas partes do sistema para se tornarem responsivas, fazendo do zero um novo layout para cozinha, etc. As ferramentas de trabalho são: Visual Studio Code, XAMPP, FileZilla, MySQL Workbench. Eu estou trabalhando com: PHP, HTML, CSS, JavaScript, BootStrap, JQuerry.",
         /** */
         "experience.ARConsultoriaLocation": "Natal, RN. Brasil.",
         "experience.ARConsultoriaPosition": "Analista de Sistemas, Foco em Front-ending.",
-        "experience.ARConsultoriaText": "Some of the things i worked on: constructing the page for the principal product of the company, the AR PLUS; I also extracted many informations from the data base to use on new graphics, I fixed lot's of layouts, corrected some bugs.The used work tools were: PHP, HTML, MySQL, CSS, Bootstrap, JavaScript, JQuery, Adianti, Highcharts, TortoiseSVN.",
+        "experience.ARConsultoriaText": "Algumas das coisas em que trabalhei: Contrui a página para o principal produto da empresa, o AR PLUS; Eu extrai diversas informações do banco de dados para contruir novos gráficos para facilitar a visualização da informação já guardada no sistema, ajeitei vários layouts, consertei alguns bugs. As ferramentas de trabalçho foram: PHP, HTML, MySQL, CSS, Bootstrap, JavaScript, JQuery, Adianti, Highcharts, TortoiseSVN.",
         /** */
         "experience.SMSLocation": "Natal, RN. Brasil.",
         "experience.SMSPosition": "Analista de Sistemas, Foco em Front-ending. Estágio remunerado.",
-        "experience.SMSText": "Internship in Secretaria Municipal de Saúde (Municipal Health Secretariat), the work got my knowledge stronger in: PHP, HTML, JavaScript, CSS, JQuerry, SQL. I worked fixing the existent systems and the help i had was by messages, since the person that could assist me was work from other places.",
+        "experience.SMSText": "Estágio na Secretaria Municipal de Saúde, o trabalho fortaleceu meu conhecimento em: PHP, HTML, JavaScript, CSS, JQuerry, SQL. eu trabalhei ajeitando e melhorando os sistemas já existentes, uma coisa importante a se relatar aqui é que minha ajuda nesse estágio foram via mensagens, já que a pessoa que podia me auxiliar estava trabalhando remotamente.",
         /** */
         "experience.SEPLANLocation": "Centro Administrativo de Natal, RN. Brasil.",
         "experience.SEPLANPosition": "Analista de dados. Estágio remunerado.",
-        "experience.SEPLANText": "Experience with Power BI and extraction of data from the data base of SEPLAN, Secretaria de Estado de Planejamento e Finanças (State Secretariat for Planning and Finance), of centro Administrativo de Natal, RN (Administrative center of Natal, RN). The search in the data base was always done in MySQL.",
+        "experience.SEPLANText": "Experiencia com Power BI e extração de dados do banco da SEPLAN (Secretaria de Estado de Planejamento e Finanças) do centro Administrativo da capital, Natal, RN. as pesquisas no banco foram feitas em MySQL.",
         /** */
         "experience.UNP1Location": "Natal, RN. Brasil.",
         "experience.UNP1Position": "Analista de Sistemas, Foco em Front-ending. Estágio não remunerado & estágio remunerado.",
-        "experience.UNP1Text": "This is the Univerdidade Potiguar, or Potiguar University in English. I participated in E-code projects, there were projects related to hospitals, to security and other minors projects, all of them were really put in use, and the financial return came back proportion a better pay to internships. The work tool were: PHP, Adianti Framework, MySQL.",
+        "experience.UNP1Text": "Esta é a Universidade Potiguar, UNP. Eu participei nos projetos do E-code, lá trsablhei em vários projetos, teve um que iria ser implantado nos hospitais publicos, teve um relacionado a segurança e vários outros projetos, todos eles foram realmente colocados em uso, houve um retorno financeiro que foi usado para melhor pagar os estagiários e criar mais vagas remuneradas. As ferramentas de trabalho foram: PHP, Adianti Framework, MySQL.",
         /** */
         "experience.UNP2Location": "Natal, RN. Brasil.",
-        "experience.UNP2Position": "Teacher and monitor. Voluntier.",
-        "experience.UNP2Text": "I patcipated of the interity of the project Informática Cidadã (citizen computing), where I teached basic computing, Windows 10 and Microsoft Office to mostly disabled people, some were hearing impaired, some visual deficiency, among some others.",
+        "experience.UNP2Position": "Professor e monitor. Voluntário.",
+        "experience.UNP2Text": "Eu participei do programa completo, o Informática Cidadã, onde eu ensinei: computação básica, Windows 10 e Microsoft Office para diversas pessoas, principalmente deficientes em alguma capacidade: alguns com deficiência auditiva, alguns com deficiência visual, e outras.",
         /** */
-        "experience.GameVicioDate": "Around 2010",
-        "experience.GameVicioLocation": "At the time the site link was: <br>www.gamevicio.com.br",
-        "experience.GameVicioPosition": "Translator. Voluntier.",
-        "experience.GameVicioText": "Voluteer work as a translator of games from english to portuguese of site gamevicio.com (between the games I helped traslate there was: Deux Ex and Fable 3). I participated in projects for a period around 2 years.",
+        "experience.GameVicioDate": "Em torno de 2010",
+        "experience.GameVicioLocation": "na época o link do site era: <br>www.gamevicio.com.br",
+        "experience.GameVicioPosition": "Tradutor. Voluntário.",
+        "experience.GameVicioText": "Me vonluntariei em diversos projetos para trabalhar na tradução de jogos do inglês para o português do site gamevicio.com (alguns dos jogos que ajudei na tradução foram: Deux Ex e Fable 3). Eu participei em diversos projetos de tradução no período aproximadamente de 2 anos.",
     }
 };
 
